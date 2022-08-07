eb7dba3deac5a50a80e7bafcc123085a975665af
cp-dev <rajeev.b@prowessenterprise.com>
2022-08-07 12:54:22 +0530

    git reset trails

--- sample.py
+++ sample.py
@@ -1,4 +1,4 @@
 print ("Hello")
 print ("again changed this line from remote replasjdflasjdfo")
 print ("demo in the day 5 class extending this line with a remote change this is ben's change")
-
+print ("git reset trails")
